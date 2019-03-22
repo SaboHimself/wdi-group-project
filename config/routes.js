859900ad17cd59ced0311fb379255ee1c0a39cb2
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-22 15:28:59 +0000

    changes to booking, auth and spaces controller. add errorhandler, changes to space & booking model

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 const spaces = require('../controllers/spaces')
+const bookings = require('../controllers/bookings')
 const secureRoute = require('../lib/secureRoute')
 const auth = require('../controllers/auth')
+
+router.route('/bookings')
+  .get(bookings.index)
+  .post(secureRoute, bookings.create)
+
+router.route('/bookings/:id')
+  .get(bookings.show)
+  .put(secureRoute, bookings.edit)
+  .delete(secureRoute, bookings.delete)
 
 router.route('/spaces')
   .get(spaces.index)
@@ -11,6 +21,9 @@ router.route('/spaces/:id')
   .get(spaces.show)
   .put(secureRoute, spaces.edit)
   .delete(secureRoute, spaces.delete)
+
+router.post('/spaces/:id/comments', secureRoute, spaces.commentCreate)
+router.delete('/spaces/:id/comments/:commentId', secureRoute, spaces.commentDelete)
 
 router.post('/register', auth.register )
 router.post('/login', auth.login )
--- controllers/spaces.js
+++ controllers/spaces.js
@@ -5,7 +5,7 @@ function indexSpace(req, res, next){
     .find()
     .populate('owner')
     .then(spaces => res.status(200).json(spaces))
-    .catch(err => console.log(err))
+    .catch(next)
 }
 
 function showSpace(req, res, next){
@@ -13,7 +13,7 @@ function showSpace(req, res, next){
     .findById(req.params.id)
     .populate('owner')
     .then(space => res.status(200).json(space))
-    .catch(err => console.log(err))
+    .catch(next)
 }
 
 function createSpace(req, res, next){
@@ -21,10 +21,9 @@ function createSpace(req, res, next){
   Space
     .create(req.body)
     .then(space => {
-      console.log(req.body)
       res.status(201).json(space)
     })
-    .catch(err => res.json(err))
+    .catch(next)
 }
 
 function editSpace(req, res, next){
@@ -37,7 +36,7 @@ function editSpace(req, res, next){
       return space.update(req.body)
     })
     .then(() => res.sendStatus(202))
-    .catch(err => console.log(err))
+    .catch(next)
 }
 
 function deleteSpace(req, res, next){
@@ -50,7 +49,34 @@ function deleteSpace(req, res, next){
       return space.remove()
     })
     .then(() => res.sendStatus(204))
-    .catch(err => console.log(err))
+    .catch(next)
+}
+
+function commentCreate(req, res, next) {
+  req.body.owner = req.currentUser
+  Space
+    .findById(req.params.id)
+    .then(space => {
+      space.comments.push(req.body)
+      return space.save()
+    })
+    .then(space => res.json(space))
+    .catch(next)
+}
+
+function commentDelete(req, res, next) {
+  Space
+    .findById(req.params.id)
+    .then(space => {
+      if (!space || !space.owner || !space.owner._id.equals(req.currentUser._id)) {
+        return res.json({ message: 'Unauthorized' })
+      }
+      const comment = space.comments.id(req.params.commentId)
+      comment.remove()
+      return space.save()
+    })
+    .then(space => res.json(space))
+    .catch(next)
 }
 
 module.exports = {
@@ -58,5 +84,7 @@ module.exports = {
   show: showSpace,
   create: createSpace,
   edit: editSpace,
-  delete: deleteSpace
+  delete: deleteSpace,
+  commentCreate,
+  commentDelete
 }
