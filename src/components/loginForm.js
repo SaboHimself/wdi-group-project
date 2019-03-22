33e13e2d1adf19753ca630c9dd86841a8288de9a
Stephen Sabo <sabo.stephen@gmail.com>
2019-03-22 14:54:49 +0000

    Update webpack config

---
+++ src/components/loginForm.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Login extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <form>
+        <label>
+        Email
+          <input/>
+        </label>
+        <label>
+        Password
+          <input />
+        </label>
+      </form>
+    )
+  }
+}
+
+export default Login
