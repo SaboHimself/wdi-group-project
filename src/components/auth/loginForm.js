8905442d5b0e3672f973b61a30f2d13279b50f5f
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-23 20:30:52 +0000

    profile page

--- src/components/auth/loginForm.js
+++ src/components/auth/loginForm.js
@@ -29,7 +29,7 @@ class Login extends React.Component {
     axios.post('/api/login', this.state.data)
       .then(res => {
         Auth.setToken(res.data.token)
-        this.props.history.push('/maps')
+        this.props.history.push('/map')
       })
       .catch(err => console.log(err))
   }
