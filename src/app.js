59fda497b3e2f47b918679e0d456395318661297
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-23 14:30:45 +0000

    made changes to front end, 1. add empty home page 2.add route to map, home, login and register

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Browser , Link, Switch, Route } from 'react-router-dom'
 
 import Map from './components/map'
 import Login from './components/auth/loginForm'
 import Register from './components/auth/registerForm'
+import Home from './components/page/home'
 
 import './stylesheets/main.scss' //main stylesheet import
 
@@ -14,11 +16,22 @@ class App extends React.Component {
 
   render() {
     return(
-      <div>
-        <Login />
-        <Register />
-        <Map />
-      </div>
+      <Browser>
+        <div>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/spaces'>Map</Link>
+            <Link to='/register'>Register</Link>
+            <Link to='/login'>login</Link>
+          </nav>
+          <Switch>
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            <Route path='/spaces' component={Map} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Browser>
     )
   }
 }
