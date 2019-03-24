9d71eb6f8e67cfa7901e80a8342f4ba1476556b4
Stephen Sabo <sabo.stephen@gmail.com>
2019-03-24 10:18:35 +0000

    Update map

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2Fib2hpbXNlbGYiLCJhIjoiY2pzcHgxeXJjMDBpbTQ5czljNHQ4dXVzMCJ9.7KpwLwJFWkQOC_RZo9jc6g'
+const bounds = [[-0.483702, 51.334679], [0.190262, 51.655070]]
 
 class Map extends React.Component {
   constructor() {
@@ -11,7 +12,12 @@ class Map extends React.Component {
     this.map = new mapboxgl.Map({
       container: this.mapDiv,
       style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: 0
+      center: {
+        lat: 51.507351,
+        lng: -0.127758
+      },
+      zoom: 15,
+      maxBounds: bounds
     })
   }
 
