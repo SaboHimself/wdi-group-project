c30f05c6bad640aa5e69fed41a795defd395d3a0
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-24 19:28:07 +0000

    move form page

--- src/components/page/home.js
+++ src/components/page/home.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
-import SpaceForm from '../spaces/new-space-forms/spaceForm'
-
 const Home = () => {
   return(
     <div>
       <h1>hello World</h1>
-      <SpaceForm />
     </div>
   )
 }
