beac5dc4fa80c2620ec765710d1aff5565b42958
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-25 13:08:15 +0000

    add search box and calendar

--- src/components/map.js
+++ src/components/map.js
@@ -45,7 +45,7 @@ class Map extends React.Component {
     this.map = new mapboxgl.Map({
       container: this.mapDiv,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: this.state.center,
+      center: [this.props.location.state.lnglat.Longitude, this.props.location.state.lnglat.Latitude],
       zoom: 15,
       maxBounds: bounds
     })
@@ -93,7 +93,7 @@ class Map extends React.Component {
   // }
 
   render() {
-    console.log(this.state.spaces)
+    console.log(this.props.location.state.lnglat.Latitude,this.props.location.state.lnglat.Longitude)
     const { spaces } = this.state
     return(
       <div>
