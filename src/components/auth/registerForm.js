8777202a1c817245973fa9a40b1a14e3a28e49de
seongjaemoon <seongjae.moon.92@gmail.com>
2019-03-27 17:11:34 +0000

    register css

--- src/components/auth/registerForm.js
+++ src/components/auth/registerForm.js
@@ -39,51 +39,54 @@ class Register extends React.Component {
 
   render() {
     return(
-      <form onSubmit={ this.handleSubmit }>
-        <label>
-        Username
-          <input
-            onChange={ this.handleChange }
-            name="username"
-            value={ this.state.data.username }
-          />
-        </label>
-        <label>
-        Email
-          <input
-            onChange={ this.handleChange }
-            name="email"
-            value={ this.state.data.email }
-          />
-        </label>
-        <label>
-        Phone Number
-          <input
-            onChange={ this.handleChange }
-            name="number"
-            value={ this.state.data.number }
-          />
-        </label>
-        <label>
-        Password
-          <input
-            onChange={ this.handleChange }
-            type="password"
-            name="password"
-            value={ this.state.data.password }
-          />
-        </label>
-        <label>
-        Password Confirmation
-          <input
-            onChange={ this.handleChange }
-            type="password"
-            name="passwordConfirmation"
-            value={ this.state.data.passwordConfirmation }
-          />
-        </label>
-        <button>Login</button>
-      </form>
+      <div className='registerForm'>
+        <div>Register</div>
+        <form onSubmit={ this.handleSubmit }>
+          <label>
+            <span>Username</span>
+            <input
+              onChange={ this.handleChange }
+              name="username"
+              value={ this.state.data.username }
+            />
+          </label>
+          <label>
+            <span>Email</span>
+            <input
+              onChange={ this.handleChange }
+              name="email"
+              value={ this.state.data.email }
+            />
+          </label>
+          <label>
+            <span>Phone Number</span>
+            <input
+              onChange={ this.handleChange }
+              name="number"
+              value={ this.state.data.number }
+            />
+          </label>
+          <label>
+            <span>Password</span>
+            <input
+              onChange={ this.handleChange }
+              type="password"
+              name="password"
+              value={ this.state.data.password }
+            />
+          </label>
+          <label>
+            <span>Password Confirmation</span>
+            <input
+              onChange={ this.handleChange }
+              type="password"
+              name="passwordConfirmation"
+              value={ this.state.data.passwordConfirmation }
+            />
+          </label>
+          <input type="submit" name="submit" value="Log in"/>
+        </form>
+      </div>
     )
   }
 }
