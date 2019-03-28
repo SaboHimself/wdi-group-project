c0d708a55ea3e5c1986a748165ac11469f89c7d2
Stephen Sabo <sabo.stephen@gmail.com>
2019-03-28 15:51:52 +0000

    Update styles

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -20,37 +20,27 @@ class Profile extends React.Component{
   render(){
     if(!this.state.user) return null
     const { user } = this.state
-    console.log(this.state)
-
     return(
-      <div className="profile-wrapper">
+      <div className="profile-wrapper animated fadeIn slower">
         <div className="profile">
-          <section className="headerSection">
-            <h1>{user.username}</h1>
-            <span>{user.email}</span>
-          </section>
-          <hr />
-          <section className="spaceSection">
-            <div>Your Spaces</div>
-            {user.userSpaces.map((space, id) => (
-              <div key={id} className="spaces">
-                <Link to={`/spaces/${space._id}`}>
-                  <div className="profile-image-wrapper">
-                    <img src={space.images[0]} />
-                  </div>
-                </Link>
-                <div className="description">
-                  <div>{space.geometry.coordinates}</div>
-                  <div>{space.price}</div>
-                  <div>{space.description}</div>
+          <div className="profile-header">
+            <p>Welcome {user.username}</p>
+          </div>
+          {user.userSpaces.map((space, id) => (
+            <div key={id} className="spaces">
+              <Link to={`/spaces/${space._id}`}>
+                <div className="profile-image-wrapper">
+                  <img src={space.images[0]} />
                 </div>
+              </Link>
+              <div className="description">
+                <div className="price">£{space.price}<span className="perHour">ph</span></div>
+                <div>{space.description}</div>
               </div>
-            ))}
-          </section>
-          <section className="bookingSection">
-            <div>Booking</div>
-          </section>
+            </div>
+          ))}
         </div>
+        <div>{user.bookings.space}</div>
       </div>
     )
   }
